9c290bc038de2e29f3819501247ac0c9a7313780
anonymous <shawn.coding.acc@gmail.com>
2021-02-05 11:21:53 +0800

    Create install_packages.sh

---
+++ install_packages.sh
@@ -0,0 +1,6 @@
+conda config --env --add channels conda-forge
+conda config --env --set channel_priority strict
+conda install python
+conda install flask
+conda install pytest
+conda install coverage
